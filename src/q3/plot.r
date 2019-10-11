e890ad6aa9761375e4deab3f783275feb0fadeec
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2019-10-11 08:50:40 -0300

    Fix csv

--- src/q3/plot.r
+++ src/q3/plot.r
@@ -1,6 +1,6 @@
 library(ggplot2)
 
 data <- read.csv(file = "./results/result.csv")
-p <- ggplot(data, aes(x=sample, y=time, color=structure)) + geom_line()
+p <- ggplot(data, aes(x=sample, y=time, color=structure)) + geom_point()
 ggsave("results/result.png", plot=p)
 
