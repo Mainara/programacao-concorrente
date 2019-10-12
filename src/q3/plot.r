7ed264463aef296d0385045fbf882ecd28052c78
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2019-10-11 22:33:03 -0300

    Add png

--- src/q3/plot.r
+++ src/q3/plot.r
@@ -4,3 +4,7 @@ data <- read.csv(file = "./results/result.csv")
 p <- ggplot(data, aes(x=sample, y=time, color=structure)) + geom_point()
 ggsave("results/result.png", plot=p)
 
+data <- read.csv(file = "./results/result_men.csv")
+p <- ggplot(data, aes(x=sample, y=usage, color=structure)) + geom_point()
+ggsave("results/result_mem.png", plot=p)
+
